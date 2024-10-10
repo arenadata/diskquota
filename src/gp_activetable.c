e70ff2265592374e6014c9f205d5bad1aa95a945
Georgy Shelkovy <g.shelkovy@arenadata.io>
2024-10-10 12:12:04 +0500

    fix

--- src/gp_activetable.c
+++ src/gp_activetable.c
@@ -375,7 +375,7 @@ gp_fetch_active_tables(bool is_init)
 
 	memset(&ctl, 0, sizeof(ctl));
 	ctl.keysize   = sizeof(Oid);
-	ctl.entrysize = sizeof(ActiveTableEntryCombined) + SEGCOUNT * sizeof(Size);
+	ctl.entrysize = offsetof(ActiveTableEntryCombined, tablesize) + (SEGCOUNT + 1) * sizeof(Size);
 	ctl.hcxt      = CurrentMemoryContext;
 
 	local_table_stats_map = diskquota_hash_create("local active table map with relfilenode info", 1024, &ctl,
