c81833bd91fafdf9b040ff93ca78eb28829ee778
Maksim Michkov <m.michkov@arenadata.io>
2024-11-20 10:51:35 +0000

    Fix tests for GPDB 7

--- tests/isolation2/sql/test_create_extension.sql
+++ tests/isolation2/sql/test_create_extension.sql
@@ -1,11 +1,17 @@
 -- check that diskquota ignores global flag appendonly=true
 
+-- for GPDB 6
 SET gp_default_storage_options='appendonly=true';
+-- for GPDB 7
+SET default_table_access_method='ao_row';
 
 CREATE EXTENSION diskquota;
 DROP EXTENSION diskquota;
 
+-- for GPDB 6
 SET gp_default_storage_options='appendonly=false';
+-- for GPDB 7
+SET default_table_access_method='heap';
 
 CREATE EXTENSION diskquota;
 
