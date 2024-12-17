38ada9689d7a75fd6fe91f577e5ddd279b5599d4
Georgy Shelkovy <g.shelkovy@arenadata.io>
2024-12-17 09:35:39 +0500

    optimize

--- src/quotamodel.c
+++ src/quotamodel.c
@@ -1387,7 +1387,6 @@ dispatch_rejectmap(StringInfoData active_oids)
 {
 	HASH_SEQ_STATUS       hash_seq;
 	GlobalRejectMapEntry *rejectmap_entry;
-	int                   num_entries, count = 0;
 	CdbPgResults          cdb_pgresults = {NULL, 0};
 	StringInfoData        rows;
 	StringInfoData        sql;
@@ -1396,15 +1395,13 @@ dispatch_rejectmap(StringInfoData active_oids)
 	initStringInfo(&sql);
 
 	LWLockAcquire(diskquota_locks.reject_map_lock, LW_SHARED);
-	num_entries = hash_get_num_entries(disk_quota_reject_map);
 	hash_seq_init(&hash_seq, disk_quota_reject_map);
 	while ((rejectmap_entry = hash_seq_search(&hash_seq)) != NULL)
 	{
+		if (rows.len > 0) appendStringInfoString(&rows, ",");
 		appendStringInfo(&rows, "ROW(%d, %d, %d, %d, %s)", rejectmap_entry->keyitem.targetoid,
 		                 rejectmap_entry->keyitem.databaseoid, rejectmap_entry->keyitem.tablespaceoid,
 		                 rejectmap_entry->keyitem.targettype, rejectmap_entry->segexceeded ? "true" : "false");
-
-		if (++count != num_entries) appendStringInfo(&rows, ",");
 	}
 	LWLockRelease(diskquota_locks.reject_map_lock);
 
@@ -1413,10 +1410,11 @@ dispatch_rejectmap(StringInfoData active_oids)
 	                 "ARRAY[%s]::diskquota.rejectmap_entry[], "
 	                 "ARRAY[%s]::oid[])",
 	                 rows.data, active_oids.data);
-	CdbDispatchCommand(sql.data, DF_NONE, &cdb_pgresults);
-
 	pfree(rows.data);
 	pfree(sql.data);
+
+	CdbDispatchCommand(sql.data, DF_NONE, &cdb_pgresults);
+
 	cdbdisp_clearCdbPgResults(&cdb_pgresults);
 }
 
