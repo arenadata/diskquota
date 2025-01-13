8fa38de888906bffa0e17b788c084142e626cc6d
Georgy Shelkovy <g.shelkovy@arenadata.io>
2025-01-13 11:46:41 +0500

    comment

--- src/gp_activetable.c
+++ src/gp_activetable.c
@@ -1047,6 +1047,11 @@ pull_active_list_from_seg(StringInfoData *active_oids)
 
 	Assert(active_oids->len == 0);
 
+	/*
+	 * Convert a hash map with oids into a string array
+	 * This is used to prepare the second array parameter
+	 * of function diskquota_fetch_table_stat.
+	 */
 	while ((oid = hash_seq_search(&iter)) != NULL)
 	{
 		if (active_oids->len > 0) appendStringInfoString(active_oids, ",");
