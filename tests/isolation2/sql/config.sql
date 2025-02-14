3185adfa69ac13cfbd55954aac83915b9aba7376
Roman Eskin <r.eskin@arenadata.io>
2025-02-14 13:57:48 +1000

    Update tests for GPDB with external Orca

--- tests/isolation2/sql/config.sql
+++ tests/isolation2/sql/config.sql
@@ -2,7 +2,7 @@
 CREATE DATABASE diskquota;
 --end_ignore
 
-!\retcode gpconfig -c shared_preload_libraries -v $(./data/current_binary_name);
+!\retcode gpconfig -c shared_preload_libraries -v "$(psql -At -c "SELECT array_to_string(array_append(string_to_array(regexp_replace(current_setting('shared_preload_libraries'), '(,{0,1})diskquota(.*)\.so', ''), ','), '$(./data/current_binary_name)'), ',')" postgres)";
 !\retcode gpconfig -c diskquota.naptime -v 0 --skipvalidation;
 !\retcode gpconfig -c max_worker_processes -v 20 --skipvalidation;
 
