7cd475a3978c366065e543b3399d14fa08fd9872
Georgy Shelkovy <g.shelkovy@arenadata.io>
2025-12-12 10:37:19 +0500

    unify

--- src/diskquota.c
+++ src/diskquota.c
@@ -91,10 +91,7 @@ static DiskQuotaWorkerEntry *volatile MyWorkerInfo = NULL;
 // how many database diskquota are monitoring on
 static int num_db = 0;
 
-#ifndef USE_ASSERT_CHECKING
-static
-#endif
-        DiskquotaLauncherShmemStruct *DiskquotaLauncherShmem = NULL;
+DiskquotaLauncherShmemStruct *DiskquotaLauncherShmem = NULL;
 
 #define MIN_SLEEPTIME 100         /* milliseconds */
 #define BGWORKER_LOG_TIME 3600000 /* milliseconds */
